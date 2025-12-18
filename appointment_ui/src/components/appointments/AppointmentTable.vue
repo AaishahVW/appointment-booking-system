94c56ec54a12f32fff4f49bfba886e27ec98d9b0
Aaishah Van Witt <aaishahvanwitt@capitecbank.co.za>
2025-12-18 13:08:09 +0200

    appointment table base layout

---
+++ appointment_ui/src/components/appointments/AppointmentTable.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+// TEMP mock
+const appointments = [
+  {
+    id: 1,
+    branch: 'Cape Town Branch',
+    date: '2025-01-12',
+    time: '10:30',
+    status: 'Confirmed',
+  },
+]
+</script>
+
+<template>
+  <div class="space-y-4">
+    <h2 class="text-xl font-medium">
+      Your Appointments
+    </h2>
+
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Branch</TableHead>
+          <TableHead>Date</TableHead>
+          <TableHead>Time</TableHead>
+          <TableHead>Status</TableHead>
+        </TableRow>
+      </TableHeader>
+
+      <TableBody>
+        <TableRow v-for="a in appointments" :key="a.id">
+          <TableCell>{{ a.branch }}</TableCell>
+          <TableCell>{{ a.date }}</TableCell>
+          <TableCell>{{ a.time }}</TableCell>
+          <TableCell>{{ a.status }}</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </div>
+</template>
