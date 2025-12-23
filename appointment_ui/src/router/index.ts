80c937482fb6b7b64a0b5bf4d15940722abff982
Aaishah Van Witt <aaishahvanwitt@capitecbank.co.za>
2025-12-23 08:57:23 +0200

    Removing all branch content to be moved to new ui

--- appointment_ui/src/router/index.ts
+++ appointment_ui/src/router/index.ts
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/pages/Home/Home.vue";
 import Appointment from "@/pages/Appoinments/Appointment.vue";
-import ConsultantView from "@/pages/Branch/ConsultantView.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/appointments", name: "Appointments", component: Appointment },
-  { path: "/branch/appointments", name: "Branch", component: ConsultantView },
 ];
 
 const router = createRouter({
