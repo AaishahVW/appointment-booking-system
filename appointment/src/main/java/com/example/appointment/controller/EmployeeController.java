a43f0a002665af2caa33ec105bf9787d3b71dad5
Aaishah Van Witt <aaishahvanwitt@capitecbank.co.za>
2025-12-31 10:22:19 +0200

    Add Employee Controller

--- appointment/src/main/java/com/example/appointment/controller/EmployeeController.java
+++ appointment/src/main/java/com/example/appointment/controller/EmployeeController.java
@@ -1,4 +1,44 @@
 package com.example.appointment.controller;
 
+import com.example.appointment.model.Employee;
+import com.example.appointment.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<Employee> create(@RequestBody Employee employee, @PathVariable UUID userId) {
+        return ResponseEntity.ok(employeeService.create(employee, userId));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getById(@PathVariable UUID id) {
+        return ResponseEntity.ok(employeeService.getById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAll() {
+        return ResponseEntity.ok(employeeService.getAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> update(@PathVariable UUID id, @RequestBody Employee employee) {
+        return ResponseEntity.ok(employeeService.update(id, employee));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        employeeService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
